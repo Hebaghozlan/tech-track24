d5e0c13fa36b15b5ba4cebf19232db3ef7c4a341
Hebaghozlan <94401320+Hebaghozlan@users.noreply.github.com>
2023-12-05 02:42:48 +0100

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,112 +1,113 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    // Data
+document.addEventListener("DOMContentLoaded", function() {
+    
+    // Valorant data
     const data = [
-    {
-        "Category": "Deulist",
-        "Agent": "Reyna",
-        "Hours": 45.0,
-        "Matches": 82.0,
-        "Picture": "https://titles.trackercdn.com/valorant-api/agents/a3bfb853-43b2-7238-a4f1-ad90e9e46bcc/displayicon.png"
-    },
-    {
-        "Category": "Deulist",
-        "Agent": "Jett",
-        "Hours": 37.0,
-        "Matches": 66.0,
-        "Picture": "https://titles.trackercdn.com/valorant-api/agents/add6443a-41bd-e414-f6ad-e58d267f4e95/displayicon.png"
-    },
-    {
-        "Category": "Sentinel",
-        "Agent": "Sage",
-        "Hours": 33.0,
-        "Matches": 59.0,
-        "Picture": "https://titles.trackercdn.com/valorant-api/agents/569fdd95-4d10-43ab-ca70-79becc718b46/displayicon.png"
-    },
-    {
-        "Category": "Deulist",
-        "Agent": "Raze",
-        "Hours": 26.0,
-        "Matches": 47.0,
-        "Picture": "https://titles.trackercdn.com/valorant-api/agents/f94c3b30-42be-e959-889c-5aa313dba261/displayicon.png"
-    },
-    {
-        "Category": "Controller",
-        "Agent": "Omen",
-        "Hours": 24.0,
-        "Matches": 41.0,
-        "Picture": "https://titles.trackercdn.com/valorant-api/agents/8e253930-4c05-31dd-1b6c-968525494517/displayicon.png"
-    },
-    {
-        "Category": "Initiator",
-        "Agent": "Gekko",
-        "Hours": 22.0,
-        "Matches": 42.0,
-        "Picture": "https://titles.trackercdn.com/valorant-api/agents/e370fa57-4757-3604-3648-499e1f642d3f/displayicon.png"
-    },
-    {
-        "Category": "Initiator",
-        "Agent": "Skye",
-        "Hours": 12.0,
-        "Matches": 23.0,
-        "Picture": "https://titles.trackercdn.com/valorant-api/agents/6f2a04ca-43e0-be17-7f36-b3908627744d/displayicon.png"
-    },
-    {
-        "Category": "Sentinel",
-        "Agent": "Deadlock",
-        "Hours": 10.0,
-        "Matches": 18.0,
-        "Picture": "https://titles.trackercdn.com/valorant-api/agents/cc8b64c8-4b25-4ff9-6e7f-37b4da43d235/displayicon.png"
-    },
-    {
-        "Category": "Sentinel",
-        "Agent": "Killjoy",
-        "Hours": 6.3,
-        "Matches": 11.0,
-        "Picture": "https://titles.trackercdn.com/valorant-api/agents/1e58de9c-4950-5125-93e9-a0aee9f98746/displayicon.png"
-    },
-    {
-        "Category": "Controller",
-        "Agent": "Brimstone",
-        "Hours": 3.9,
-        "Matches": 7.0,
-        "Picture": "https://titles.trackercdn.com/valorant-api/agents/9f0d8ba9-4140-b941-57d3-a7ad57c6b417/displayicon.png"
-    },
-    {
-        "Category": "Deulist",
-        "Agent": "Neon",
-        "Hours": 3.6,
-        "Matches": 7.0,
-        "Picture": "https://titles.trackercdn.com/valorant-api/agents/bb2a4828-46eb-8cd1-e765-15848195d751/displayicon.png"
-    },
-    {
-        "Category": "Initiator",
-        "Agent": "Fade",
-        "Hours": 1.0,
-        "Matches": 2.0,
-        "Picture": "https://titles.trackercdn.com/valorant-api/agents/dade69b4-4f5a-8528-247b-219e5a1facd6/displayicon.png"
-    },
-    {
-        "Category": "Initiator",
-        "Agent": "Sova",
-        "Hours": 0.6,
-        "Matches": 1.0,
-        "Picture": "https://titles.trackercdn.com/valorant-api/agents/320b2a48-4d9b-a075-30f1-1f93a9b638fa/displayicon.png"
-    },
-    {
-        "Category": "Deulist",
-        "Agent": "Yoru",
-        "Hours": 0.4,
-        "Matches": 1.0,
-        "Picture": "https://titles.trackercdn.com/valorant-api/agents/7f94d92c-4234-0a36-9646-3a87eb8b5c89/displayicon.png"
-    },
-    {
-        "Category": "Sentinel",
-        "Agent": "Chamber",
-        "Hours": 0.3,
-        "Matches": 1.0,
-        "Picture": "https://titles.trackercdn.com/valorant-api/agents/22697a3d-45bf-8dd7-4fec-84a9e28c69d7/displayicon.png"
-    }
-];
+        {
+            "Category": "Deulist",
+            "Agent": "Reyna",
+            "Hours": 45.0,
+            "Matches": 82.0,
+            "Picture": "https://titles.trackercdn.com/valorant-api/agents/a3bfb853-43b2-7238-a4f1-ad90e9e46bcc/displayicon.png"
+        },
+        {
+            "Category": "Deulist",
+            "Agent": "Jett",
+            "Hours": 37.0,
+            "Matches": 66.0,
+            "Picture": "https://titles.trackercdn.com/valorant-api/agents/add6443a-41bd-e414-f6ad-e58d267f4e95/displayicon.png"
+        },
+        {
+            "Category": "Sentinel",
+            "Agent": "Sage",
+            "Hours": 33.0,
+            "Matches": 59.0,
+            "Picture": "https://titles.trackercdn.com/valorant-api/agents/569fdd95-4d10-43ab-ca70-79becc718b46/displayicon.png"
+        },
+        {
+            "Category": "Deulist",
+            "Agent": "Raze",
+            "Hours": 26.0,
+            "Matches": 47.0,
+            "Picture": "https://titles.trackercdn.com/valorant-api/agents/f94c3b30-42be-e959-889c-5aa313dba261/displayicon.png"
+        },
+        {
+            "Category": "Controller",
+            "Agent": "Omen",
+            "Hours": 24.0,
+            "Matches": 41.0,
+            "Picture": "https://titles.trackercdn.com/valorant-api/agents/8e253930-4c05-31dd-1b6c-968525494517/displayicon.png"
+        },
+        {
+            "Category": "Initiator",
+            "Agent": "Gekko",
+            "Hours": 22.0,
+            "Matches": 42.0,
+            "Picture": "https://titles.trackercdn.com/valorant-api/agents/e370fa57-4757-3604-3648-499e1f642d3f/displayicon.png"
+        },
+        {
+            "Category": "Initiator",
+            "Agent": "Skye",
+            "Hours": 12.0,
+            "Matches": 23.0,
+            "Picture": "https://titles.trackercdn.com/valorant-api/agents/6f2a04ca-43e0-be17-7f36-b3908627744d/displayicon.png"
+        },
+        {
+            "Category": "Sentinel",
+            "Agent": "Deadlock",
+            "Hours": 10.0,
+            "Matches": 18.0,
+            "Picture": "https://titles.trackercdn.com/valorant-api/agents/cc8b64c8-4b25-4ff9-6e7f-37b4da43d235/displayicon.png"
+        },
+        {
+            "Category": "Sentinel",
+            "Agent": "Killjoy",
+            "Hours": 6.3,
+            "Matches": 11.0,
+            "Picture": "https://titles.trackercdn.com/valorant-api/agents/1e58de9c-4950-5125-93e9-a0aee9f98746/displayicon.png"
+        },
+        {
+            "Category": "Controller",
+            "Agent": "Brimstone",
+            "Hours": 3.9,
+            "Matches": 7.0,
+            "Picture": "https://titles.trackercdn.com/valorant-api/agents/9f0d8ba9-4140-b941-57d3-a7ad57c6b417/displayicon.png"
+        },
+        {
+            "Category": "Deulist",
+            "Agent": "Neon",
+            "Hours": 3.6,
+            "Matches": 7.0,
+            "Picture": "https://titles.trackercdn.com/valorant-api/agents/bb2a4828-46eb-8cd1-e765-15848195d751/displayicon.png"
+        },
+        {
+            "Category": "Initiator",
+            "Agent": "Fade",
+            "Hours": 1.0,
+            "Matches": 2.0,
+            "Picture": "https://titles.trackercdn.com/valorant-api/agents/dade69b4-4f5a-8528-247b-219e5a1facd6/displayicon.png"
+        },
+        {
+            "Category": "Initiator",
+            "Agent": "Sova",
+            "Hours": 0.6,
+            "Matches": 1.0,
+            "Picture": "https://titles.trackercdn.com/valorant-api/agents/320b2a48-4d9b-a075-30f1-1f93a9b638fa/displayicon.png"
+        },
+        {
+            "Category": "Deulist",
+            "Agent": "Yoru",
+            "Hours": 0.4,
+            "Matches": 1.0,
+            "Picture": "https://titles.trackercdn.com/valorant-api/agents/7f94d92c-4234-0a36-9646-3a87eb8b5c89/displayicon.png"
+        },
+        {
+            "Category": "Sentinel",
+            "Agent": "Chamber",
+            "Hours": 0.3,
+            "Matches": 1.0,
+            "Picture": "https://titles.trackercdn.com/valorant-api/agents/22697a3d-45bf-8dd7-4fec-84a9e28c69d7/displayicon.png"
+        }
+    ];    
 
     // Width & height of the visualization
     const margin = { top: 30, right: 150, bottom: 70, left: 60 },
@@ -137,48 +138,50 @@ document.addEventListener("DOMContentLoaded", function(event) {
     svg.append("g")
         .attr("transform", `translate(0,${height})`)
         .call(xAxis);
-   // X axis label:
-    svg.append("text")
-        .attr("text-anchor", "end")
-        .attr("x", width / 2 + margin.left +285)
-        .attr("y", height + margin.top + 20)
-        .text("Matches");
+
+    // X axis label:
+        svg.append("text")
+            .attr("text-anchor", "end")
+            .attr("x", width / 2 + margin.left +285)
+            .attr("y", height + margin.top + 20)
+            .text("Matches");
 
 
     // Y Axis
-    svg.append("g")
-        .call(yAxis);
+        svg.append("g")
+            .call(yAxis);
   
   // Y axis label:
-    svg.append("text")
-        .attr("text-anchor", "end")
-        .attr("transform", "rotate(-90)")
-        .attr("y", -margin.left+15 )
-        .attr("x", -margin.top+10)
-        .text("Hours Played");
+        svg.append("text")
+            .attr("text-anchor", "end")
+            .attr("transform", "rotate(-90)")
+            .attr("y", -margin.left+15 )
+            .attr("x", -margin.top+10)
+            .text("Hours Played");
 
-    // Tooltip
-    const tooltip = d3.select("body").append("div")
-        .attr("id", "tooltip")
-        .style("opacity", 0);
+  // Tooltip
+        const tooltip = d3.select("body").append("div")
+            .attr("id", "tooltip")
+            .style("opacity", 0);
 
-    // Filter function
-    window.filterCategory = function(category) {
+
+  // Filter function
+        window.filterCategory = function(category) {
         let filteredData = category === 'All' ? data : data.filter(d => d.Category === category);
 
-        // Filter
+  // Filter
         dots = svg.selectAll(".dot")
             .data(filteredData, d => d.Agent);
 
-        // Remove unneeded data
+  // Remove unneeded data
         dots.exit().remove();
 
-        // Filter new data
+  // Filter new data
         const dotsEnter = dots.enter().append("circle")
             .attr("class", "dot")
             .attr("r", 6);
 
-        // Enter and update
+  // Enter and update
         dotsEnter.merge(dots)
             .attr("cx", d => xScale(d.Matches))
             .attr("cy", d => yScale(d.Hours))
@@ -192,7 +195,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 }
             });
 
-        // Hover
+  // Hover
         dotsEnter.on("mouseover", function(event, d) {
             tooltip.transition()
                 .duration(200)
@@ -201,15 +204,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 .style("left", (event.pageX) + "px")
                 .style("top", (event.pageY - 28) + "px");
         });
-        // Fade
+  // Fade
         dotsEnter.on("mouseout", function() {
             tooltip.transition()
                 .duration(500)
                 .style("opacity", 0);
         });
-        
-    };
+  
+};
 
-    // Refresh
+  // Refresh
     filterCategory('All');
 });
+
+  // Smooth scroll to visualization
+    function scrollToVisualization() {
+    document.getElementById('visualization').scrollIntoView({ behavior: 'smooth' });
+}
